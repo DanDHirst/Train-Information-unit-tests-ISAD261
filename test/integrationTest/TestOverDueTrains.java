4e656a005025afe0f3dd5de9a113e12f60af037b
Dan Hirst <danhirst123@gmail.com>
2019-12-31 17:21:23 +0000

    Intergration test for get the overdue trains

--- test/integrationTest/TestOverDueTrains.java
+++ test/integrationTest/TestOverDueTrains.java
@@ -25,7 +25,7 @@ import webconnect.WebConnection;
  */
 public class TestOverDueTrains {
     private WebConnection Con;
-    private ArrayList<TrainInfo> trainAtStation;
+    private ArrayList<TrainInfo> trainOverdue;
     private TrainInfomation trainStation;
     public TestOverDueTrains() {
         try {
@@ -58,18 +58,45 @@ public class TestOverDueTrains {
     //
      @Test
      public void TestIfMethodOutputsInfomation(){
-        try {
-            TrainInfomation trainStation = new TrainInfomation();
-            ArrayList<String> trainOverdue = trainStation.getOverdueTrains("Plymouth", 1100);
+            trainOverdue = trainStation.getOverdueTrains("Plymouth", 1100);
             if(trainOverdue.size() != 0){
                 assertTrue(true);
             }
             else{
                 assertTrue("No data is returned", false);
             }
-        } catch (ParseException ex) {
-            Logger.getLogger(TestOverDueTrains.class.getName()).log(Level.SEVERE, null, ex);
-        }
+
      }
-    
+     
+    @Test
+    public void TestProgramOutputsValidString(){
+        trainOverdue = this.trainStation.getOverdueTrains("Saltash", 1050);
+        assertEquals("Check message is valid", "The train arriving at Saltash for 1041 is Overdue. We are sorry for any inconvenience this may have caused you!", trainOverdue.get(0));
+        
+    } 
+    @Test
+    public void TestProgramInvalidStationTime(){
+        trainOverdue = this.trainStation.getOverdueTrains("Plymouths", 2000);
+        assertTrue("check if the method returns null if there is no overdue trains", trainOverdue.size() == 0);
+    }
+    @Test
+    public void TestCheckIfNumberOfOverdueTrainsIsCorrect(){
+        trainOverdue = this.trainStation.getOverdueTrains("Liskeard", 1500);
+        assertTrue("check if the method returns all the overdue trains", trainOverdue.size() == 2);
+    }
+    @Test
+    public void TestCheckDoesntCrashWhenDispatchIsNull(){
+        trainOverdue = this.trainStation.getOverdueTrains("Penzance", 1500);
+        assertTrue("check if the method returns all the overdue train that have null at the end", trainOverdue.size() >= 1);
+    }
+    @Test
+    public void TestCheckTheEdgeCasesOfTheTime(){
+        trainOverdue = this.trainStation.getOverdueTrains("St Germans", 1047);
+        assertEquals("time is 1047 so should not be late",trainOverdue.size(), 0);
+        trainOverdue = this.trainStation.getOverdueTrains("St Germans", 1048);
+        assertEquals("time is 1048 so should not be late",trainOverdue.size(), 0);
+        trainOverdue = this.trainStation.getOverdueTrains("St Germans", 1049);
+        assertEquals("time is 1049 so should be late",trainOverdue.size(),1);
+        
+    }
 }
