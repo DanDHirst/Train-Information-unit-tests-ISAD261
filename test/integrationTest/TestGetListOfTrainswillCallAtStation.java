a4b426368661f4a89434ad03d4c5646f40484a7b
Dan Hirst <danhirst123@gmail.com>
2019-12-31 17:25:31 +0000

    Intergration test for get trains that call by

--- test/integrationTest/TestGetListOfTrainswillCallAtStation.java
+++ test/integrationTest/TestGetListOfTrainswillCallAtStation.java
@@ -67,4 +67,51 @@ public class TestGetListOfTrainswillCallAtStation {
             }
        
     }
+    public void TestCheckCorrectValues(){
+        trainAtcall = trainStation.getListOfTrainswillCallAtStation("Plymouth", "Liskeard");
+        assertEquals("check name is Plymouth", trainAtcall.get(0).getName(), "Plymouth");
+        assertEquals("check arrival time is 1029", trainAtcall.get(0).getArrives(), "1029");
+        assertEquals("check depart time is 1033", trainAtcall.get(0).getDeparts(), "1033");
+        
+        assertEquals("check name is Liskeard", trainAtcall.get(1).getName(), "Liskeard");
+        assertEquals("check arrival time is 1100", trainAtcall.get(1).getArrives(), "1100");
+        assertEquals("check depart time is 1101", trainAtcall.get(1).getDeparts(), "1101");
+
+    }
+    @Test 
+    public void TestCheckOrderOfTimes(){
+        trainAtcall = trainStation.getListOfTrainswillCallAtStation("Teignmouth", "Dawlish");
+        assertEquals("check arrival time is 1028", trainAtcall.get(0).getArrives(), "1028");
+        assertEquals("check depart time is 1029", trainAtcall.get(0).getDeparts(), "1029");
+        
+        assertEquals("check arrival time is 1033", trainAtcall.get(1).getArrives(), "1033");
+        assertEquals("check depart time is 1034", trainAtcall.get(1).getDeparts(), "1034");
+    }
+    
+    @Test 
+    public void TestCheckTheSourceAndDestinationSize(){
+        trainAtcall = trainStation.getListOfTrainswillCallAtStation("Saltash", "Par");
+        if (trainAtcall.size() % 2 ==0) {
+            assertTrue("There is an even amount of objects", true);
+        }
+        else{
+            assertTrue("There is an odd number of objects", false);
+        }
+    }
+    @Test 
+    public void TestCheckIfOutputShouldBeNull(){
+        trainAtcall = trainStation.getListOfTrainswillCallAtStation("2323523", "45345");
+        if (trainAtcall.size() ==0) {
+            assertTrue("The program returned null", true);
+        }
+        else{
+            assertTrue("The program returned items when it shouldnt have", false);
+        }
+    }
+    @Test 
+    public void TestCheckIfMissedOutAnyTrains(){
+        trainAtcall = trainStation.getListOfTrainswillCallAtStation("Birmingham New Street", "Stockport");
+        assertEquals("There should only be two objects for this call", trainAtcall.size(),2);
+    }
+    
 }
